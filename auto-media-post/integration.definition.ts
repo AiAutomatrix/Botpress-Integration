26373472135e67b26eefcf009ac6310c5bec2dfb
Garret Friesen <138935870+AiAutomatrix@users.noreply.github.com>
2024-06-20 19:25:27 +0000

    Fix node-sass build issues and update dependencies

---
+++ auto-media-post/integration.definition.ts
@@ -0,0 +1,81 @@
+import { IntegrationDefinition } from '@botpress/sdk'
+import { integrationName } from './package.json'
+import z from 'zod';
+
+export default new IntegrationDefinition({
+  name: integrationName,
+  version: '0.0.1',
+  readme: 'hub.md',
+  icon: 'icon.svg',
+  configuration: {
+    schema: z.object({
+      webhookUrl: z.string().describe('The url to post the bot answers to.'),
+      Publishablekey: z.string(),
+      Secretkey: z.string(),
+    }),
+  },
+  events: {
+    webhook: {
+      schema: z.object({ id: z.string() }),
+    },
+  },
+  actions: {
+    facebook: {
+      input: {
+        schema: z.object({
+          facebookCode: z.string(),
+        }),
+      },
+      output: {
+        schema: z.object({ code: z.string(), htmlPath: z.string() }),
+      },
+    },
+  },
+  channels: {
+    webhook: {
+      conversation: {
+        tags: {
+          id: { title: 'Conversation ID', description: 'The ID of the conversation' },
+        },
+      },
+      messages: {
+        text: {
+          schema: z.object({
+            text: z.string(),
+          }),
+        },
+      },
+    },
+    comment: {
+      messages: {
+        text: {
+          schema: z.object({ text: z.string() }),
+        },
+      },
+      message: {
+        tags: {
+          id: {
+            title: 'Message ID',
+            description: 'Message ID from Stripe',
+          },
+        },
+      },
+      conversation: {
+        tags: {
+          taskId: {
+            title: 'Task ID',
+            description: 'Task ID from Stripe',
+          },
+        },
+      },
+    },
+  },
+  user: {
+    tags: {
+      id: {
+        title: 'User ID',
+        description: 'User ID from Stripe',
+      },
+    },
+  },
+});
